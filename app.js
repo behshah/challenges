be204a51183796ed63972b8e743b2c942b275b8b
behshah <behshah26@yahoo.com>
2020-09-02 09:47:07 +0800

    app.js modified

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-// // toFixed : return a strig ,with a number written with the specified number of decimals.
+// clone in git test
+// toFixed : return a strig ,with a number written with the specified number of decimals.
 // var a = 9.656;
 // var x = a.toFixed() ;
 // var y = a.toFixed(2) ;
