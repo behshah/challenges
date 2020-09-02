b9a7ff5f1e64533390c0b477b464bd2619d7dc68
behshah <behshah26@yahoo.com>
2020-09-02 08:03:28 +0800

    second change

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 const form = document.querySelector('#addForm');
 
 form.addEventListener('submit', function(e){
-//  var a = "jhon";;
-//  var x = Number(a);
     e.preventDefault();
+//  var a = "jhon";;
+//var cars = ("pride", "sab" , "honda", "merci","wv");
+// cars[3] = "topol"
+// var person = {firstName:"John", lastName:"Doe", age:46};
+//  var x = Number(a);
 
-    const textForm = document.querySelector('#item').value;
+ var cars = ["volvo", "pride", "mitsubishi","cadilac"];
+    cars.push("suzuki");
+    // var  text = "<ul>";
+    // cars.forEach(myFunction);
+    // text += "</ul>";
+    // function myFunction (value){
+    //     text += "<li>"+ value + "</li>"
+    // }
+
+    // const textForm = document.querySelector('#item').value;
     //const useMethod = textForm;
     // const useMethod = Number(textForm); 
     // const useMethod = parseInt(textForm); 
     // const useMethod = parseFloat(textForm); 
     // const useMethod = new Number(textForm)
-    const useMethod = textForm.toExponential(2);
+    // const useMethod = textForm.toExponential(2);
     // var useMethod = Number.MAX_VALUE; 
     //      useMethod = Number.MIN_VALUE; 
     //      useMethod = Number.POSITIVE_INFINITY; 
@@ -26,9 +38,10 @@ form.addEventListener('submit', function(e){
     var li = document.createElement(li);
 
     li.className = "list-group-item";
-    li.appendChild(document.createTextNode(useMethod));
+    li.appendChild(document.createTextNode(cars));
     ulItems.appendChild(li);
-    console.log(useMethod);
+    console.log(typeof(cars))
+    console.log(cars);
 
     
 
